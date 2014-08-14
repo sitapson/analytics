fdbef37b51a92b1ad8dd48afdfce1eae3ffcc113
Simon <simon.tapson@groundsix.com>
2014-08-14 14:10:39 +0100

    Added db env var

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
+			'database'  => 'analytics',
 			'username'  => 'forge',
-			'password'  => '',
+			'password'  => getenv('db_password'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
